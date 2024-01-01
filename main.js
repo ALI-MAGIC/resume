22afdd76ee112bf7a35fba0ac827c33a90eda7e3
Ali-Magic <130074124+ALI-MAGIC@users.noreply.github.com>
2024-01-01 03:18:14 -0800

    Update main.js

--- main.js
+++ main.js
@@ -10,3 +10,16 @@ function showMenu() {
         document.querySelector(".menu-burger-icon i").setAttribute('class', 'fa fa-bars');
     }
 }
+
+function showMenuFa(){
+    var navMenuFa = document.getElementById("nav-menuFa");
+    if (navMenuFa.className === "header-menuFa") {
+        navMenuFa.className += " show";
+        document.querySelector(".menu-burger-iconFa i").style = "color : #fff";
+        document.querySelector(".menu-burger-iconFa i").setAttribute('class', 'fa fa-close');
+    } else {
+        navMenuFa.className = "header-menuFa";
+        document.querySelector(".menu-burger-iconFa i").style = "color : #c76a00";
+        document.querySelector(".menu-burger-iconFa i").setAttribute('class', 'fa fa-bars');
+    }
+}
